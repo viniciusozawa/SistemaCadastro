ff359792e0250985bcecd42c272e92467ad1a89b
VINICIUS YUJI OZAWA <02338079698@campus.machado.mch>
2025-08-27 10:05:12 -0300

    subindo

---
+++ src/modelo/Cidade.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author 02338079698
+ */
+public class Cidade {
+    private String nome;
+    private static int cod;
+
+    public Cidade() {
+        cod++;
+    }
+    
+    
+}
